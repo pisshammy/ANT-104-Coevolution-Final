c0b73fa5224d7859b617e09e52fa8744c268973c
pisshammy <bamhamyt@gmail.com>
2026-06-09 02:40:59 -0500

    feat: github pages deploy workflow

--- README.md
+++
@@ -1,1 +0,0 @@
-# ANT 104 Final Project 
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 export default defineConfig({
-  site: "http://localhost:4321",
-  base: "/",
+  site: "https://pisshammy.github.io",
+  base: "main",
   integrations: [tailwind()],
 });
