adedfdeb1bca4c9383833702b81009d96c258110
pisshammy <bamhamyt@gmail.com>
2026-06-09 03:02:44 -0500

    Fix GitHub Pages Astro deployment paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
-  site: "https://pisshammy.github.io",
-  base: "main",
+  site: "https://pisshammy.github.io/ANT-104-Coevolution-Final",
+  base: "/ANT-104-Coevolution-Final",
   integrations: [tailwind()],
 });
